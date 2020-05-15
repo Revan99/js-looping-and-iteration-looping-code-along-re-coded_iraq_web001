5c71fbf62d3098077636f85f9aed2f4e9d95f5bf
Revan Sarbast <revansarbast8@gmail.com>
2020-05-15 13:19:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,9 @@ function countDown(num){
   }
 }
 console.log(countDown(-10));
+
+function writeCards(name){
+  for(let a = 0; a < name.lenth; a++){
+    console.log('thank you ${name[a]}');
+  }  
+}
